0d630f9f3d61b173b9d974bfdd0b3a6743ddb305
alberto <albertocarrascofernandez@hotmail.com>
2021-03-03 15:18:44 +0100

    a

--- P.2 carrito compra con flitros de compra/js.js
+++ P.2 carrito compra con flitros de compra/js.js
@@ -1,85 +1,113 @@
 var data = {
-    el: '#example-3',
-      ver: false,
-      preciomax :999 ,//muestra todos los cursos
-      cesta:[],
-      añadeProducto:true,
-      VerCurso:true,
-      VerMaterial:false,
-  
-  
-      total:"",
-    cursos: [
-      {
+el: '#example-3',
+    ver: false,
+    preciomax :999 ,//muestra todos los cursos
+    cesta:[],
+    añadeProducto:true,/*oculta texto cesta */
+    VerCurso:true,
+    VerMaterial:false,
+
+
+    total:"",
+cursos: [
+    {
+      "id":1,
+      "name": "Curso Wordpress",
+      "imagen": "IMG/wordpress.jpg",
+      "desc": "El curso más potente para aprender todo lo necesario sobre Wordpress",
+      "precio": 110.10,
+      "stock":"",
+      "color":null,
+    },
+    {
+      "id":2,
+      "name": "Curso Front End",
+      "imagen": "IMG/fullstack.jpg",
+      "desc": "El curso más potente para aprender todo lo necesario sobre Frontend ",
+      "precio": 220.20,
+      "stock":"",
+      "color":null,
+    } ,
+    {
+      "id":3,
+      "name": "Curso Fullstack",
+      "imagen": "IMG/frontend.jpg",
+      "desc": "El curso más potente para aprender todo lo necesario sobre Frontend y Backend ",
+      "precio": 420,
+      "stock":"",
+      "color":null,
+    } ,
+    { 
+      "id":4,
+      "name": "Curso SEO",
+      "imagen": "IMG/seo.png",
+      "desc": "El curso más potente para aprender todo lo necesario sobre SEO y posicionamiento Web",
+      "precio": 300,
+      "stock":"",
+      "color":null,
+    } 
+  ],
+materialEscolar:[
+    {
         "id":1,
-        "name": "Curso Wordpress",
-        "imagen": "IMG/wordpress.jpg",
-        "desc": "El curso más potente para aprender todo lo necesario sobre Wordpress",
-        "precio": 110.10,
+        "name": "Bolígrafos",
+        "imagen": "IMG/bolis.jpg",
+        "desc": "Bolígrafos personalizados",
+        "precio": 1,
         "stock":"",
         "color":null,
       },
       {
         "id":2,
-        "name": "Curso Front End",
-        "imagen": "IMG/fullstack.jpg",
-        "desc": "El curso más potente para aprender todo lo necesario sobre Frontend ",
-        "precio": 220.20,
+        "name": "subcarpeta",
+        "imagen": "IMG/subcarpeta.jpg",
+        "desc": "Subcarpeta Exacompta din A4",
+        "precio": 3,
         "stock":"",
         "color":null,
-      } ,
+      },
       {
         "id":3,
-        "name": "Curso Fullstack",
-        "imagen": "IMG/frontend.jpg",
-        "desc": "El curso más potente para aprender todo lo necesario sobre Frontend y Backend ",
-        "precio": 420,
+        "name": "Gomas",
+        "imagen": "IMG/gomas.jpg",
+        "desc": "Gomas 430",
+        "precio": 1.20,
         "stock":"",
         "color":null,
-      } ,
-      { 
-        "id":4,
-        "name": "Curso SEO",
-        "imagen": "IMG/seo.png",
-        "desc": "El curso más potente para aprender todo lo necesario sobre SEO y posicionamiento Web",
-        "precio": 300,
-        "stock":"",
-        "color":null,
-      } 
-    ],
-    materialEscolar:[
+      },
       {
-          "id":1,
-          "name": "Bolígrafos",
-          "imagen": "IMG/bolis.jpg",
-          "desc": "Bolígrafos personalizados",
-          "precio": 1,
-          "stock":"",
-          "color":null,
+        "id":4,
+        "name": "Papel",
+        "imagen": "IMG/papel.jpg",
+        "desc": "Papel Din A4 80gr 500 Hojas",
+        "precio": 5.5,
+        "stock":"",
+        "color":null,
+      },
+  ]
+}
+var app = new Vue({
+el: '#app',
+    filters:{
+        moneda:function (valor) {  
+          return Number.parseFloat(valor).toFixed(2) + "€";
+         },
+        dosDigitos(value, pattern = ""){ // dos digitos
+          return value.toString().padStart(2,pattern)
+       }
+      },
+    methods:{
+        enlaceCurso:function(){
+            this.VerCurso=true; 
+            this.VerMaterial= false;
+            return  this.VerCurso, this.VerMaterial
         },
-        {
-          "id":2,
-          "name": "Cuadernos",
-          "imagen": "IMG/cuadernos.jpg",
-          "desc": "Cuaderno de espiral, A4, 90 x 140 mm, 50 hojas",
-          "precio": 3,
-          "stock":"",
-          "color":null,
+        enlaceMaterial:function(){
+            this.VerCurso=false; 
+            this.VerMaterial=true;
+            return  this.VerCurso, this.VerMaterial
         },
-    ]
-  }
-  var app = new Vue({
-        el: '#app',
-        filters:{
-          moneda:function (valor) {  
-            return Number.parseFloat(valor).toFixed(2) + "€";
-           },
-          dosDigitos(value, pattern = ""){ // dos digitos
-            return value.toString().padStart(2,pattern)
-         }
-        },
-        methods:{
-          agregarCesta:function(curso){
+        agregarCesta:function(curso){
             /*al crear una array dentro del array debe ser cambiado en la vista*/
             var prodExistente;
             var exitente;
@@ -90,18 +118,16 @@ var data = {
               }else {
                 return  false;
               }
-             });
-  
-             if(exitente.length){
+            });    
+            if(exitente.length){
                this.cesta[prodExistente]
-             }else{
+            }else{
                this.cesta.push({curso:curso})
-             }
-           
+            }
             /*filtro pra no repetir cursos*/
-          },
-          random:function(curso){
-            /*de forma random me pone si esta o no es stock y su color (cambia el stock según clicas el boton aunque no agregues elementos en la cesta)*/
+        },
+        random:function(curso){
+            /*de forma random me pone si esta o no es stock y su color (cambia el stock según clicas el boton aunque  no agregues elementos en la cesta)*/
             var  colorStock
             var dado=  Math.floor(Math.random()* 2)+1; 
             console.log(`ramdon ${dado}`)
@@ -110,6 +136,8 @@ var data = {
               dado ="En stock";
               curso.stock=dado;
               curso.color= colorStock;
+             /*    materialmaterial.stock=dado;
+                material.color= colorStock; */
               console.log(`color ${colorStock}`)
               return dado,colorStock
             }else{
@@ -117,11 +145,13 @@ var data = {
               dado = "Sin stock"
               curso.stock=dado;
               curso.color= colorStock;
+                // materialmaterial.stock=dado;
+                // material.color= colorStock;
               console.log(`color ${colorStock}`)
               return dado,colorStock
             }
-          },
-  
         },
-        data: data,
-      })
+
+    },
+    data: data,
+})
