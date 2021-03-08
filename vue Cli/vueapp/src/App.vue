215880bbfa02ebfaf2f11a938fe10df0de7bdc3c
alberto <albertocarrascofernandez@hotmail.com>
2021-03-08 19:39:21 +0100

    import animatecss

--- vue Cli/vueapp/src/App.vue
+++ vue Cli/vueapp/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
+  <button type="button" class="btn btn-primary">Primary</button>
   <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
--- vue Cli/vueapp/src/main.js
+++ vue Cli/vueapp/src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css"
+import "animate.css/source/animate.css"
+
 
 createApp(App).mount("#app");
